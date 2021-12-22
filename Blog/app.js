16a067dc10c140624d84a987ecfc6e1dc5533e85
directstar1 <ishanzuaim@gmail.com>
2021-12-22 23:03:36 +1100

    Added blog

---
+++ Blog/app.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
+const { render } = require("express/lib/response");
+
+const app = express();
+
+//connect to mongoDB;
+const dbURL =
+  "mongodb+srv://ishanblog:test1234@cluster0.mlzmf.mongodb.net/blog-tut?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => {
+    console.log(err);
+  });
+
+//registers the view engine
+app.set("view engine", "ejs");
+
+//middleware static files
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+//basic routes
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+//blog routes
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+  blog.save().then((result) => {
+    res.redirect("/blogs");
+  });
+});
+
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  // Yes, it's a valid ObjectId, proceed with `findById` call.
+
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog Details" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" });
+    })
+    .catch((err) => console.log(err));
+});
+
+//404 Page
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
